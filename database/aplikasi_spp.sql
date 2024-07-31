914d480cbc5dcf61a29e78afdbf2340cda6af229
Ayu Pertiwie-RPL 2 <116324869+ayu-rpl@users.noreply.github.com>
2024-07-31 15:32:45 +0700

    Add files via upload

---
+++ database/aplikasi_spp.sql
@@ -0,0 +1,199 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 31, 2024 at 08:41 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `aplikasi_spp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kelas`
+--
+
+CREATE TABLE `kelas` (
+  `id_kelas` int(11) NOT NULL,
+  `nama_kelas` varchar(10) NOT NULL,
+  `kompetensi_keahlian` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `kelas`
+--
+
+INSERT INTO `kelas` (`id_kelas`, `nama_kelas`, `kompetensi_keahlian`) VALUES
+(123, 'DQWD111111', 'TAV'),
+(686, 'RPL 2', 'RPL'),
+(2323, 'RPL 2', 'RPL'),
+(11220, 'TAV', 'Teknik Audio Video'),
+(12121, 'TKR 12', 'Pilih Kompetensi Keahlian');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembayaran`
+--
+
+CREATE TABLE `pembayaran` (
+  `id_pembayaran` int(11) NOT NULL,
+  `id_petugas` int(11) NOT NULL,
+  `nisn` varchar(10) NOT NULL,
+  `tgl_bayar` date DEFAULT NULL,
+  `bulan_dibayar` varchar(8) NOT NULL,
+  `tahun_dibayar` varchar(4) NOT NULL,
+  `id_spp` int(11) NOT NULL,
+  `jumlah_bayar` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `petugas`
+--
+
+CREATE TABLE `petugas` (
+  `id_petugas` int(11) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `nama_petugas` varchar(35) NOT NULL,
+  `level` enum('admin','petugas',',') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `petugas`
+--
+
+INSERT INTO `petugas` (`id_petugas`, `username`, `password`, `nama_petugas`, `level`) VALUES
+(22, 'ayuayu@1', '202cb962ac59075b964b07152d234b70', 'HalooAyu1', 'admin'),
+(42, 'apaaa@1', 'c20ad4d76fe97759aa27a0c99bff6710', 'Ayu', 'petugas'),
+(1211, 'ayu123@iya', '202cb962ac59075b964b07152d234b70', 'Ayu', 'admin'),
+(10001, 'admin@aplikasi.spp.id', '21232f297a57a5a743894a0e4a801fc3', 'Admin', 'admin'),
+(20002, 'petugas@aplikasi.sppd.id', '202cb962ac59075b964b07152d234b70', 'Petugas', 'petugas'),
+(32321, 'yu@iyaaok', '385d04e7683a033fcc6c6654529eb7e9', 'iya', 'petugas'),
+(100202, 'apaaaaaaaaaaa', '00e11252db1051387c47521767296b42', 'siapa', 'petugas'),
+(342355, 'apaaaaaaaaaaa', 'd42c62acd0fc46e4674cfaaec298edcb', 'PETUGAS', 'petugas');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `siswa`
+--
+
+CREATE TABLE `siswa` (
+  `nisn` varchar(10) NOT NULL,
+  `nis` varchar(15) NOT NULL,
+  `nama` varchar(35) NOT NULL,
+  `id_kelas` int(11) NOT NULL,
+  `alamat` text NOT NULL,
+  `no_telp` varchar(13) NOT NULL,
+  `id_spp` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `siswa`
+--
+
+INSERT INTO `siswa` (`nisn`, `nis`, `nama`, `id_kelas`, `alamat`, `no_telp`, `id_spp`) VALUES
+('348573', '5346356', 'siapa aza', 11220, 'sdfgrtdjhytrfjn', '2482347932656', 12),
+('45765484', '65878', 'trghbrt', 686, 'tghbrt', '8979807087078', 13);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `spp`
+--
+
+CREATE TABLE `spp` (
+  `id_spp` int(11) NOT NULL,
+  `nominal` int(11) NOT NULL,
+  `tahun` int(11) NOT NULL,
+  `keterangan` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `spp`
+--
+
+INSERT INTO `spp` (`id_spp`, `nominal`, `tahun`, `keterangan`) VALUES
+(12, 132324, 4234, 'adaaaaaaaaaaaa'),
+(13, 13414134, 211, 'yyayayayaya'),
+(3234, 23294, 211, 'ADAA');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `kelas`
+--
+ALTER TABLE `kelas`
+  ADD PRIMARY KEY (`id_kelas`);
+
+--
+-- Indexes for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD PRIMARY KEY (`id_pembayaran`),
+  ADD KEY `id_petugas` (`id_petugas`),
+  ADD KEY `nisn` (`nisn`),
+  ADD KEY `id_spp` (`id_spp`);
+
+--
+-- Indexes for table `petugas`
+--
+ALTER TABLE `petugas`
+  ADD PRIMARY KEY (`id_petugas`);
+
+--
+-- Indexes for table `siswa`
+--
+ALTER TABLE `siswa`
+  ADD PRIMARY KEY (`nisn`),
+  ADD KEY `id_kelas` (`id_kelas`),
+  ADD KEY `id_spp` (`id_spp`);
+
+--
+-- Indexes for table `spp`
+--
+ALTER TABLE `spp`
+  ADD PRIMARY KEY (`id_spp`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD CONSTRAINT `pembayaran_ibfk_1` FOREIGN KEY (`id_petugas`) REFERENCES `petugas` (`id_petugas`),
+  ADD CONSTRAINT `pembayaran_ibfk_2` FOREIGN KEY (`nisn`) REFERENCES `siswa` (`nisn`),
+  ADD CONSTRAINT `pembayaran_ibfk_3` FOREIGN KEY (`id_spp`) REFERENCES `spp` (`id_spp`);
+
+--
+-- Constraints for table `siswa`
+--
+ALTER TABLE `siswa`
+  ADD CONSTRAINT `siswa_ibfk_1` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`),
+  ADD CONSTRAINT `siswa_ibfk_2` FOREIGN KEY (`id_spp`) REFERENCES `spp` (`id_spp`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
